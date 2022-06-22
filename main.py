39a3e38af2c107ec3c15f4f3ce8d671dcb9d7681
Ethan Lew <107879719+ethxnsan@users.noreply.github.com>
2022-06-22 17:34:10 +0000

    guessing game

--- main.py
+++ main.py
@@ -1,2 +1,8 @@
 import random
-computerNumber = 
+computerNumber = random.randint(0, 10)
+userGuess = int(input("Guess what number I am thinking of between 0-10:"))                              
+if (userGuess == computerNumber) :
+  print("You guessed correctly!")
+else:
+  print("Wrong! My number was "+ str(computerNumber) + ".")
+  
